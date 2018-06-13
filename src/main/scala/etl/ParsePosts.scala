a9b2d8cdb836bbf01a88c0911ef6ca8ba9ff3b95
Bogusz <stefanczykb@gmail.com>
2018-06-13 18:47:03 +0200

    moved ParsePosts object from Post file

---
+++ src/main/scala/etl/ParsePosts.scala
@@ -0,0 +1,49 @@
+package etl
+
+import net.ruippeixotog.scalascraper.model.{Document, Element}
+import net.ruippeixotog.scalascraper.dsl.DSL._
+import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
+
+
+object ParsePosts
+{
+  def fromDoc(doc: Document): List[Post] = {
+    doc
+      .extract(elementList("#content .post"))
+      .flatMap(elem => elementToPost(elem))
+  }
+
+
+  private def elementToPost(postElement: Element) =
+    try{
+      Some(Post(
+        id = readPostId(postElement),
+        points = readPostPoints(postElement),
+        content = escapedPostContent(postElement)
+      ))
+    }
+    catch{
+      case _: IllegalArgumentException => None
+      case _: NoSuchElementException => None
+    }
+
+  private def escapedPostContent(postElement: Element) = {
+    (postElement >> element(".post-content")).innerHtml
+  }
+
+  private def readPostPoints(postElement: Element) = {
+    raw"-?\d+".r
+      .findFirstMatchIn(postElement >> text(".bar .points"))
+      .getOrElse("")
+      .toString
+      .toLong
+  }
+
+  private def readPostId(postElement: Element) = {
+    raw"\d+".r
+      .findFirstMatchIn(postElement >> text(".bar .qid"))
+      .getOrElse("")
+      .toString
+      .toLong
+  }
+}
--- src/test/scala/etl/ParsePostsSpec.scala
+++ src/test/scala/etl/ParsePostsSpec.scala
@@ -4,7 +4,7 @@ import org.scalatest._
 import net.ruippeixotog.scalascraper.browser.JsoupBrowser
 
 
-class PostTransformationSpec extends FlatSpec with Matchers {
+class ParsePostsSpec extends FlatSpec with Matchers {
 
   private def getDoc(filename: String) = JsoupBrowser().parseFile(s"src/test/resources/$filename")
 
